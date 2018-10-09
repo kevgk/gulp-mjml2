f0338d10d3e573bcc79fba85b46f0661bf5fb77a
Kevin Gerken <kevin_gerken@live.de>
2018-10-09 18:07:08 +0200

    improved errorhandling

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const through = require('through2');
 const mjml2html = require('mjml').default;
+const PluginError = require('plugin-error');
 
 module.exports = options => {
   return through.obj(function(file, encoding, cb) {
-    const content = file.contents.toString();
-    const html = mjml2html(content, options).html;
-    file.contents = new Buffer(html);
-    this.push(file);
+
+    const PLUGINNAME = 'gulp-mjml2';
+
+    if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			cb(new PluginError(PLUGINNAME, 'Streaming not supported'));
+			return;
+		}
+
+    try {
+			const content = file.contents.toString();
+      const html = mjml2html(content, options).html;
+      file.contents = new Buffer(html);
+      this.push(file);
+		} catch (err) {
+			this.emit('error', new PluginError(PLUGINNAME, err));
+		}
+
     cb();
   });
 };
+
+module.exports.mjml = mjml2html;
